07c290b2d2fcdf38edcc4e8cd627c21a73de0284
donggoolosori <kdj6551@naver.com>
2022-03-14 15:03:34 +0900

    feat: remark를 이용해 markdown 내용을 html로 파싱

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
 
 export interface PostData {
   id: string;
@@ -46,14 +48,21 @@ export function getAllPostIds() {
   });
 }
 
-export function getPostData(id: string) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDir, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   const matterResult = matter(fileContents);
 
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+
+  const contentHtml = processedContent.toString();
+
   return {
     id,
+    contentHtml,
     ...matterResult.data,
   };
 }
