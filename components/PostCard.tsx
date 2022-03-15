ca6ba59dddb2ba529ebb1d4f11c8813e4f577e8e
donggoolosori <kdj6551@naver.com>
2022-03-15 21:45:31 +0900

    chore: PostCard 넓이 변경 및, border radius 변경

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -11,14 +11,14 @@ interface Props {
 export default function PostCard({ id, title, date, coverImg }: Props) {
   return (
     <Link href={`/posts/${id}`}>
-      <a className="w-96 h-96">
-        <div className="flex flex-col gap-2 justify-evenly h-full w-full relative p-10 text-white">
+      <a className="w-100 h-96">
+        <div className="flex flex-col gap-2 justify-evenly h-full w-full relative p-10 text-slate-50">
           <Image
             src={coverImg}
             alt="cover-image"
             layout="fill"
             objectFit="cover"
-            className="rounded-lg -z-10 brightness-75"
+            className="-z-10 brightness-75 rounded-3xl"
           />
           <h1 className="font-bold text-3xl">{title}</h1>
           <h3 className="text-sm">
