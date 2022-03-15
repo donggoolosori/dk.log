feeb3f0c3a50588d90668edcc9fb023678ce4404
donggoolosori <kdj6551@naver.com>
2022-03-15 21:35:51 +0900

    feat: PostCard에 배경 이미지 삽입

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface Props {
@@ -10,12 +11,20 @@ interface Props {
 export default function PostCard({ id, title, date, coverImg }: Props) {
   return (
     <Link href={`/posts/${id}`}>
-      <a className="flex flex-col gap-2 justify-between border-2 rounded-lg w-2/5 h-36 p-4">
-        <h1 className="font-bold text-2xl">{title}</h1>
-
-        <h3 className="text-sm">
-          <p className="italic font-thin">{date}</p>
-        </h3>
+      <a className="w-96 h-96">
+        <div className="flex flex-col gap-2 justify-evenly h-full w-full relative p-10 text-white">
+          <Image
+            src={coverImg}
+            alt="cover-image"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-lg -z-10 brightness-75"
+          />
+          <h1 className="font-bold text-3xl">{title}</h1>
+          <h3 className="text-sm">
+            <p className="italic font-thin">{date}</p>
+          </h3>
+        </div>
       </a>
     </Link>
   );
