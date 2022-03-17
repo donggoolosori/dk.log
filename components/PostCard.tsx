88a0f393ff0592ce5c48d8c097c839cd3df338be
donggoolosori <kdj6551@naver.com>
2022-03-17 19:02:56 +0900

    feat: blur 이미지 적용
    이미지 로딩 시 blur 이미지를 보여준다

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,25 +1,45 @@
+import { PostData } from '@lib/posts';
 import Image from 'next/image';
 import Link from 'next/link';
 
-interface Props {
-  id: string;
-  title: string;
-  date: string;
-  coverImg: string;
-}
+type Props = PostData;
 
-export default function PostCard({ id, title, date, coverImg }: Props) {
+export default function PostCard({
+  id,
+  title,
+  date,
+  coverImg,
+  blurCss,
+}: Props) {
   return (
     <Link href={`/posts/${id}`}>
       <a className="w-100 h-96 rounded-3xl">
         <div className="flex flex-col gap-2 justify-evenly h-full w-full relative p-10 text-slate-50 hover:-translate-y-2 transition-transform duration-300">
-          <Image
-            src={coverImg}
-            alt="cover-image"
-            layout="fill"
-            objectFit="cover"
-            className="rounded-3xl brightness-50"
-          />
+          <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full overflow-hidden rounded-3xl">
+            <div
+              style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                transform: 'scale(1.5)',
+                filter: 'blur(40px)',
+                ...blurCss,
+              }}
+            />
+
+            <Image
+              src={coverImg}
+              alt="cover-image"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-3xl brightness-50"
+            />
+          </div>
+
           <section className="z-10 w-full h-full flex flex-col justify-evenly">
             <h1 className="font-bold text-3xl">{title}</h1>
             <h3 className="text-sm">
