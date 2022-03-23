c3b65e1c39b95c8ec794dc178fdd49b4e49203dc
donggoolosori <kdj6551@naver.com>
2022-03-23 17:13:27 +0900

    feat: 포스트카드 hover 시 카드 밝기 밝게

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -15,8 +15,8 @@ export default function PostCard({
 }: Props) {
   return (
     <Link href={`/posts/${id}`}>
-      <a className="h-80 min-w-full md:min-w-[40%] lg:min-w-[30%] xl:h-96 rounded-3xl flex flex-col flex-1 gap-2 justify-evenly relative hover:-translate-y-2 transition-transform duration-300">
-        <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-50">
+      <a className="group h-80 min-w-full md:min-w-[40%] lg:min-w-[30%] xl:h-96 rounded-3xl flex flex-col flex-1 gap-2 justify-evenly relative hover:-translate-y-2 transition-transform duration-300">
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-50 group-hover:brightness-75">
           <div className="relative w-full h-full">
             <BlurImage blurCss={blurCss} />
             <Image
