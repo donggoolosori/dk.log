9fff1fb1eb06f221858b4864183cfb4887a8c9d8
donggoolosori <kdj6551@naver.com>
2022-03-30 21:07:39 +0900

    chore: Post 이미지에 그림자 스타일 추가

--- components/PostCardWrapper/PostCardWrapper.tsx
+++ components/PostCardWrapper/PostCardWrapper.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function PostCardList({ allPostsData }: Props) {
   return (
-    <div className="flex flex-wrap gap-6">
+    <div className="flex flex-wrap gap-8">
       {allPostsData.map((props) => (
         <PostCard key={props.id} {...props} />
       ))}
