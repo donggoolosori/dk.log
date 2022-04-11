3f74107ab50fce959088dc2fc60d6410148a2489
donggoolosori <kdj6551@naver.com>
2022-04-11 19:58:50 +0900

    feat: 헤더 화면 상단에 고정

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,17 +4,19 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <nav className="flex flex-row justify-between items-end mb-10">
-      <div className="text-3xl">
-        <Link href={'/'}>
-          <a>{blogName}</a>
-        </Link>
-      </div>
-      <div className="text-xl">
-        <div>
-          <Link href={'/posts'}>
-            <a>Posts</a>
+    <nav className="fixed top-0 left-0 bg-white z-10 w-full h-14 opacity-70 flex justify-center items-center">
+      <div className="flex flex-row justify-between items-center w-full h-full max-w-7xl px-4">
+        <div className="text-3xl">
+          <Link href={'/'}>
+            <a>{blogName}</a>
           </Link>
+        </div>
+        <div className="text-xl">
+          <div>
+            <Link href={'/posts'}>
+              <a>Posts</a>
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
--- components/layout.tsx
+++ components/layout.tsx
@@ -14,6 +14,7 @@ export default function Layout({ children }: LayoutProps) {
         <meta name="og:title" content={siteTitle} />
       </Head>
       <Header />
+      <div className="mb-20"></div>
       {children}
     </div>
   );
