dc0e58cf90003e64c9eae55d86ce024d709ca97e
donggoolosori <kdj6551@naver.com>
2022-04-12 16:49:10 +0900

    feat: 프로필 카드의 3d 애니메이션 구현
    - 카드가 마우스 커서에 따라 3d로 회전

--- components/ProfileCard/ProfileCard.tsx
+++ components/ProfileCard/ProfileCard.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
 const ProfileCard = () => {
+  const cardWrapper = useRef<HTMLDivElement>(null);
+  const card = useRef<HTMLElement>(null);
+
+  const onMouseMove = useCallback((e: React.MouseEvent) => {
+    if (!cardWrapper.current) return;
+
+    const rect = cardWrapper.current.getBoundingClientRect();
+
+    const xAxis = rect.left + rect.width / 2;
+    const yAxis = rect.top + rect.height / 2;
+
+    const rotateY = (xAxis - e.pageX) / 10;
+    const rotateX = (yAxis - e.pageY) / 10;
+
+    if (card.current) {
+      card.current.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+    }
+  }, []);
+
+  const onMouseLeave = useCallback(() => {
+    if (card.current) {
+      card.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    }
+  }, []);
+
   return (
-    <section className="max-w-[680px] h-100 bg-slate-400 mb-20 mx-auto rounded-3xl"></section>
+    <div
+      ref={cardWrapper}
+      className="max-w-[440px] h-[620px] mb-20 mx-auto flex items-center justify-center"
+      onMouseMove={onMouseMove}
+      onMouseLeave={onMouseLeave}
+    >
+      <section
+        ref={card}
+        className="w-100 h-[580px] bg-slate-400 rounded-3xl"
+      ></section>
+    </div>
   );
 };
 
