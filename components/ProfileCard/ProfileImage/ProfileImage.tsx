e2a439d0002d73afa405737cae30f2c86781a416
donggoolosori <kdj6551@naver.com>
2022-04-12 22:42:55 +0900

    feat: 프로필 이미지 컴포넌트 구현

---
+++ components/ProfileCard/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,18 @@
+import { profileImage } from '@constants/profile';
+import Image from 'next/image';
+
+const ProfileImage = () => {
+  return (
+    <div className="relative w-full h-1/2 shadow-md shadow-sky-300">
+      <Image
+        src={profileImage}
+        alt="profile-image"
+        priority
+        layout="fill"
+        objectFit="cover"
+      />
+    </div>
+  );
+};
+
+export default ProfileImage;
