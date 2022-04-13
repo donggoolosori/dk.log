9836ded264144523d79924a0f42c4f066f500e90
donggoolosori <kdj6551@naver.com>
2022-04-13 19:29:41 +0900

    chore: 포스트 카드 이미지 밝기 올림

--- components/PostCardWrapper/PostCard/PostCard.tsx
+++ components/PostCardWrapper/PostCard/PostCard.tsx
@@ -15,7 +15,7 @@ export default function PostCard({
   return (
     <Link href={`/posts/${id}`}>
       <a className="group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 justify-evenly relative hover:-translate-y-2 transition-transform duration-300 shadow-xl shadow-slate-500">
-        <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-50 group-hover:brightness-75">
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75 group-hover:brightness-75">
           <PostImage blurCss={blurCss} coverImg={coverImg} />
         </div>
         <section className="z-10 w-full h-4/5 mt-20 flex flex-col justify-between p-10 text-slate-50">
