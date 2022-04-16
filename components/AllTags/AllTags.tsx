a2f69fd1d20d12dc6ac062b067d0cc48396914f9
donggoolosori <kdj6551@naver.com>
2022-04-17 00:22:03 +0900

    feat: 태그 요소들의 배경에 그라데이션 배경화면 적용

--- components/AllTags/AllTags.tsx
+++ components/AllTags/AllTags.tsx
@@ -10,7 +10,12 @@ const AllTags = ({ allTags }: Props) => {
   return (
     <div className="flex flex-wrap gap-2 mb-20 max-w-3xl">
       {allTags.map((tag, idx) => (
-        <Tag key={tag[0] + idx} tag={tag[0]} count={tag[1]} className="px-2" />
+        <Tag
+          key={tag[0] + idx}
+          tag={tag[0]}
+          count={tag[1]}
+          className="py-[1px] bg-fixed bg-gradient-to-r from-[#7F7FD5] via-[#86A8E7] to-[#91eae4] text-slate-50 border-none"
+        />
       ))}
     </div>
   );
