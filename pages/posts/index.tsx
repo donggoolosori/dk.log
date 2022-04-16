657d9a59ac7e9ce09c651fc4ad60cff4ffad8bf2
donggoolosori <kdj6551@naver.com>
2022-04-16 23:05:20 +0900

    feat: 전체 태그 데이터를 posts 페이지에 static props로 넘겨줌

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,26 +1,33 @@
+import AllTags from '@components/AllTags';
 import Layout from '@components/layout';
 import PostCardWrapper from '@components/PostCardWrapper';
 import { getSortedPostsMetaData, PostData } from '@lib/posts';
+import { getAllTags, Tags } from '@lib/tag';
 import { GetStaticProps } from 'next';
 
 interface Props {
-  allPostsData: PostData[];
+  allPostsMetaData: PostData[];
+  allTags: Tags;
 }
 
-export default function Posts({ allPostsData }: Props) {
+export default function Posts({ allPostsMetaData, allTags }: Props) {
   return (
     <Layout>
-      <PostCardWrapper allPostsData={allPostsData} />
+      <AllTags allTags={allTags} />
+      <PostCardWrapper allPostsMetaData={allPostsMetaData} />
     </Layout>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = await getSortedPostsMetaData();
+  const allPostsMetaData = await getSortedPostsMetaData();
+
+  const allTags = getAllTags(allPostsMetaData);
 
   return {
     props: {
-      allPostsData,
+      allPostsMetaData,
+      allTags,
     },
   };
 };
