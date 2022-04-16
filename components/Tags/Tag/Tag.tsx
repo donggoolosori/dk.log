761ddc31f860c13bda3a97e0c92ceb6d565af2cf
donggoolosori <kdj6551@naver.com>
2022-04-16 23:44:53 +0900

    refactor: Tag 컴포넌트에 className props 추가
    - Tag 컴포넌트를 사용할 때 className에 tailwind 스타일 적용 가능

--- components/Tags/Tag/Tag.tsx
+++ components/Tags/Tag/Tag.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 interface Props {
   tag: string;
   count?: number;
+  className?: string;
 }
 
-const Tag = ({ tag, count }: Props) => {
+const Tag = ({ tag, count, className }: Props) => {
   return (
-    <li className="border-2 rounded-xl px-3 flex gap-2">
+    <li className={`border-2 rounded-xl px-3 flex gap-2 ${className}`}>
       <span>{tag}</span>
       {count && <span className="text-xs">{count}</span>}
     </li>
