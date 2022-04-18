1dbc19a0ac97938dd851358b93ec38c82d5cd2bc
donggoolosori <kdj6551@naver.com>
2022-04-18 23:22:43 +0900

    chore: 포스트카드의 태그에 호버 시, 배경색 변경

--- components/Tags/Tags.tsx
+++ components/Tags/Tags.tsx
@@ -8,7 +8,7 @@ const Tags = ({ tags }: Props) => {
   return (
     <ul className="text-sm flex justify-start items-center gap-3 w-full h-6 overflow-scroll">
       {tags?.map((tag, idx) => (
-        <Tag key={tag + idx} tag={tag} />
+        <Tag key={tag + idx} tag={tag} className="hover:bg-blue-400" />
       ))}
     </ul>
   );
