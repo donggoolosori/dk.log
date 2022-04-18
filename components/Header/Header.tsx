11c387097eb090482368eb316bdbc363daa64326
donggoolosori <kdj6551@naver.com>
2022-04-19 00:08:33 +0900

    dark mode: Header 글씨 색 설정

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = () => {
   return (
     <nav className="fixed top-0 left-0 bg-white dark:bg-slate-800 z-10 w-full h-14 opacity-70 flex justify-center items-center">
-      <div className="flex flex-row justify-between items-center w-full h-full max-w-7xl px-4 text-3xl dark:text-white">
+      <div className="flex flex-row justify-between items-center w-full h-full max-w-7xl px-4 text-3xl">
         <div className="font-bold">
           <Link href={'/'}>
             <a>{blogName}</a>
