c8dc70b7ad149883a3d85618a959c3a0faa188bf
donggoolosori <kdj6551@naver.com>
2022-04-18 14:35:55 +0900

    fix: 포스트 카드 링크 내부에 태그 링크 오류 해결

--- components/PostCardWrapper/PostCard/PostCard.tsx
+++ components/PostCardWrapper/PostCard/PostCard.tsx
@@ -15,20 +15,20 @@ export default function PostCard({
   tags,
 }: Props) {
   return (
-    <Link href={`/posts/${id}`}>
-      <a className="group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 justify-evenly relative hover:-translate-y-2 transition-transform duration-300 shadow-xl shadow-slate-500">
-        <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75">
+    <Link href={`/posts/${id}`} passHref>
+      <div className="cursor-pointer group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 justify-evenly relative hover:-translate-y-2 transition-transform duration-300 shadow-xl shadow-slate-500">
+        <a className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75">
           <PostImage blurCss={blurCss} coverImg={coverImg} />
-        </div>
+        </a>
         <section className="z-10 w-full h-4/5 mt-20 flex flex-col justify-between p-10 text-slate-50">
           {<Tags tags={tags} />}
-          <h1 className="font-bold text-2xl w-full line-clamp-2 sm:line-clamp-1 md:line-clamp-2">
+          <a className="font-bold text-2xl w-full line-clamp-2 sm:line-clamp-1 md:line-clamp-2">
             {title}
-          </h1>
-          <p className="text-md w-full line-clamp-2">{description}</p>
-          <span className="text-md italic h-1/5">{date}</span>
+          </a>
+          <a className="text-md w-full line-clamp-2">{description}</a>
+          <a className="text-md italic h-1/5">{date}</a>
         </section>
-      </a>
+      </div>
     </Link>
   );
 }
