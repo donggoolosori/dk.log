bdb01638bb4d2123e5605609e8629938e53140c5
donggoolosori <kdj6551@naver.com>
2022-04-19 00:08:43 +0900

    dark mode: 포스트카드 텍스트 색 설정

--- components/PostCardWrapper/PostCard/PostCard.tsx
+++ components/PostCardWrapper/PostCard/PostCard.tsx
@@ -16,8 +16,8 @@ export default function PostCard({
 }: Props) {
   return (
     <Link href={`/posts/${id}`} passHref>
-      <div className="cursor-pointer group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 justify-evenly relative hover:-translate-y-2 transition-transform duration-300 shadow-xl shadow-slate-500">
-        <a className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75">
+      <div className="cursor-pointer group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 justify-evenly relative hover:-translate-y-2 transition-transform duration-300 shadow-xl shadow-slate-500 dark:shadow-slate-700">
+        <a className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75 dark:brightness-50">
           <PostImage blurCss={blurCss} coverImg={coverImg} />
         </a>
         <section className="z-10 w-full h-4/5 mt-20 flex flex-col justify-between p-10 text-slate-50">
