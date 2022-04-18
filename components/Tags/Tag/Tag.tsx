bfae5b0f68f5e389e740686d7e4926096ebc122b
donggoolosori <kdj6551@naver.com>
2022-04-18 14:28:09 +0900

    feat: Tag 컴포넌트에 링크 기능 추가

--- components/Tags/Tag/Tag.tsx
+++ components/Tags/Tag/Tag.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface Props {
   tag: string;
   count?: number;
@@ -6,12 +8,14 @@ interface Props {
 
 const Tag = ({ tag, count, className }: Props) => {
   return (
-    <li
-      className={`rounded-xl px-3 flex gap-2 cursor-pointer border-2 ${className}`}
-    >
-      <span>{tag}</span>
-      {count && <span className="text-xs">{count}</span>}
-    </li>
+    <Link href={`/posts?tag=${tag}`}>
+      <a
+        className={`rounded-xl px-3 flex gap-2 cursor-pointer border-2 ${className}`}
+      >
+        <span>{tag}</span>
+        {count && <span className="text-xs">{count}</span>}
+      </a>
+    </Link>
   );
 };
 
