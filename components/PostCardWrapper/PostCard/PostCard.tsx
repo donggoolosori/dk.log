5e50070a78066f5194ee1c31f4e13ee71f16d384
donggoolosori <kdj6551@naver.com>
2022-04-19 20:11:25 +0900

    feat: 포스팅카드의 본문 영역 css 수정

--- components/PostCardWrapper/PostCard/PostCard.tsx
+++ components/PostCardWrapper/PostCard/PostCard.tsx
@@ -16,17 +16,17 @@ export default function PostCard({
 }: Props) {
   return (
     <Link href={`/posts/${id}`} passHref>
-      <div className="cursor-pointer group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 justify-evenly relative hover:-translate-y-2 transition-transform duration-300 shadow-xl dark:shadow-lg shadow-slate-500 dark:shadow-black">
+      <div className="cursor-pointer group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 relative hover:-translate-y-2 transition-transform duration-300 shadow-xl dark:shadow-lg shadow-slate-500 dark:shadow-black">
         <a className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75 dark:brightness-50">
           <PostImage blurCss={blurCss} coverImg={coverImg} />
         </a>
-        <section className="z-10 w-full h-4/5 mt-20 flex flex-col justify-between p-10 text-slate-50">
+        <section className="z-10 w-full h-full flex flex-col justify-end gap-8 px-10 pb-8 text-slate-50">
           {<Tags tags={tags} />}
           <a className="font-bold text-2xl w-full line-clamp-2 sm:line-clamp-1 md:line-clamp-2">
             {title}
           </a>
           <a className="text-md w-full line-clamp-2 max-w-2xl">{description}</a>
-          <a className="text-md italic h-1/5">{date}</a>
+          <a className="text-md italic">{date}</a>
         </section>
       </div>
     </Link>
