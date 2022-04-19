ec63ffd0d5095be889e3aa7492da58a71b407aab
donggoolosori <kdj6551@naver.com>
2022-04-19 17:02:27 +0900

    chore: items end 설정

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
             <a>{blogName}</a>
           </Link>
         </div>
-        <div className="text-xl flex justify-end gap-6 items-center">
+        <div className="text-xl flex justify-end gap-6 items-end">
           <div className="hover:text-indigo-500 dark:hover:text-indigo-300">
             <Link href={'/posts'}>
               <a>Posts</a>
