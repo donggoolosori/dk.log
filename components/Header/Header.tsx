844598988d09007cef2cc7a174d6f6420aa789a6
donggoolosori <kdj6551@naver.com>
2022-04-19 13:32:31 +0900

    chore: header의 css 수정
    - z-index 올림
    - transition color 속성 부여

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const { isDark, darkModeHandler } = useDarkMode();
 
   return (
-    <nav className="fixed top-0 left-0 bg-white dark:bg-slate-800 z-10 w-full h-14 opacity-70 flex justify-center items-center">
+    <nav className="fixed top-0 left-0 bg-white dark:bg-slate-800 opacity-70 z-50 w-full h-14 flex justify-center items-center transition-colors">
       <div className="flex flex-row justify-between items-center w-full h-full max-w-7xl px-4 text-3xl">
         <div className="font-bold">
           <Link href={'/'}>
