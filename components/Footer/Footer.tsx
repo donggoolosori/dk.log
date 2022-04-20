5a0a01509955bf9d9b739e4509c53bba03bab5fb
donggoolosori <kdj6551@naver.com>
2022-04-20 22:21:05 +0900

    chore: footer에 margin bottom 추가

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="w-full flex justify-center mt-20 text-slate-600 dark:text-white">
+    <div className="w-full flex justify-center mt-20 mb-10 text-slate-600 dark:text-white">
       <p>{`Copyright © 2022 • ${siteTitle} - ${blogDesc}`}</p>
     </div>
   );
