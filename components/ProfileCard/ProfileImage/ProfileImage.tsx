9e0a3c8938d373899be55299781522918c39f1f0
donggoolosori <kdj6551@naver.com>
2022-04-20 15:16:43 +0900

    chore: 다크모드의 프로필카드 그림자색 indigo로 변경

--- components/ProfileCard/ProfileImage/ProfileImage.tsx
+++ components/ProfileCard/ProfileImage/ProfileImage.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const ProfileImage = () => {
   return (
-    <div className="relative w-full h-1/2 shadow-md shadow-sky-200 dark:shadow-purple-700">
+    <div className="relative w-full h-1/2 shadow-md shadow-sky-200 dark:shadow-indigo-500">
       <Image
         src={profileImage}
         alt="profile-image"
