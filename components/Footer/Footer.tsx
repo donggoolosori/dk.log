c62eb9d2fc74d43a32ce022cd3ebbf89eedf72c8
donggoolosori <kdj6551@naver.com>
2022-04-21 12:46:59 +0900

    feat: footer에 아이콘 링크 추가

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,27 @@
 import { blogDesc, siteTitle } from '@constants/blog';
-import React from 'react';
+import { email, githubURL, instagramURL } from '@constants/profile';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Footer = () => {
   return (
-    <div className="w-full flex justify-center mt-20 mb-10 text-slate-600 dark:text-white">
-      <p>{`Copyright © 2022 • ${siteTitle} - ${blogDesc}`}</p>
+    <div className="flex flex-col mt-20 mb-10 text-slate-600 dark:text-white gap-4">
+      <div className="flex justify-center gap-4">
+        <a href={githubURL}>
+          <GitHubIcon />
+        </a>
+        <a href={`mailto:${email}`}>
+          <EmailIcon />
+        </a>
+        <a href={instagramURL}>
+          <InstagramIcon />
+        </a>
+      </div>
+      <div className="w-full flex justify-center">
+        <p>{`Copyright © 2022 • ${siteTitle} - ${blogDesc}`}</p>
+      </div>
     </div>
   );
 };
