62b95b577e370c6978b5438eb793bc1079be0906
donggoolosori <kdj6551@naver.com>
2022-04-21 17:10:45 +0900

    fix: route가 변경되면 utteraces의 script 재실행

--- components/Comment/Comment.tsx
+++ components/Comment/Comment.tsx
@@ -1,21 +1,27 @@
 import { label, repo } from '@constants/utterances';
+import { useRouter } from 'next/router';
 import { useEffect, useRef } from 'react';
 
 const Comment = () => {
   const commentRef = useRef<HTMLElement>(null);
+
+  const route = useRouter().asPath;
 
   useEffect(() => {
     const scriptElem = document.createElement('script');
     scriptElem.src = 'https://utteranc.es/client.js';
     scriptElem.async = true;
     scriptElem.setAttribute('repo', `${repo}`);
-    scriptElem.setAttribute('issue-term', 'pathname');
+    scriptElem.setAttribute('issue-term', 'url');
     scriptElem.setAttribute('theme', 'photon-dark');
     scriptElem.setAttribute('label', `${label}`);
     scriptElem.crossOrigin = 'anonymous';
 
+    if (commentRef.current) {
+      commentRef.current.innerHTML = '';
+    }
     commentRef.current?.appendChild(scriptElem);
-  }, []);
+  }, [route]);
 
   return <section ref={commentRef} />;
 };
