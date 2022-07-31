4d72c7e1baa7267bb431762b2d885b77c848d2d7
Dongjune Dane Kim <kdj6551@naver.com>
2022-07-31 16:05:31 +0900

    feat: href가 없는 a태그를 div로 수정

--- components/PostCardList/PostCard/PostCard.tsx
+++ components/PostCardList/PostCard/PostCard.tsx
@@ -18,16 +18,18 @@ const PostCard: FC<Props> = ({
   return (
     <Link href={`/posts/${id}`} passHref>
       <div className="cursor-pointer group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 relative shadow-xl dark:shadow-lg shadow-slate-500 dark:shadow-black">
-        <a className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75 dark:brightness-50">
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75 dark:brightness-50">
           <PostImage blurCss={blurCss} coverImg={coverImg} />
-        </a>
+        </div>
         <section className="z-10 w-full h-full flex flex-col justify-end gap-8 px-10 pb-8 text-slate-50">
           {<Tags tags={tags} />}
-          <a className="font-bold text-2xl w-full line-clamp-2 sm:line-clamp-1 md:line-clamp-2">
+          <div className="font-bold text-2xl w-full line-clamp-2 sm:line-clamp-1 md:line-clamp-2">
             {title}
-          </a>
-          <a className="text-md w-full line-clamp-2 max-w-2xl">{description}</a>
-          <a className="text-md italic">{date}</a>
+          </div>
+          <div className="text-md w-full line-clamp-2 max-w-2xl">
+            {description}
+          </div>
+          <div className="text-md italic">{date}</div>
         </section>
       </div>
     </Link>
