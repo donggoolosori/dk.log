c202081ec7ff6542894e58ce6d678219b345d511
donggoolosori <kdj6551@naver.com>
2023-01-08 00:48:42 +0900

    feat: update posting list design

--- components/PostCardList/PostCard/PostCard.tsx
+++ components/PostCardList/PostCard/PostCard.tsx
@@ -1,34 +1,29 @@
-import { PostMetaData } from '@lib/posts';
-import Link from 'next/link';
-import PostImage from '@components/PostImage';
-import Tags from '@components/Tags';
-import { FC } from 'react';
+import { PostMetaData } from "@lib/posts";
+import Link from "next/link";
+import PostImage from "@components/PostImage";
+import Tags from "@components/Tags";
+import { FC } from "react";
 
 type Props = PostMetaData;
 
-const PostCard: FC<Props> = ({
-  id,
-  title,
-  date,
-  coverImg,
-  description,
-  blurCss,
-  tags,
-}) => {
+const PostCard: FC<Props> = ({ id, title, date, coverImg, description, blurCss, tags }) => {
   return (
-    <Link href={`/posts/${id}`} passHref>
-      <div className="cursor-pointer group min-w-full md:min-w-[40%] xl:min-w-[30%] h-96 rounded-3xl flex flex-col flex-1 gap-2 relative shadow-xl dark:shadow-lg shadow-slate-500 dark:shadow-black">
+    <Link
+      href={`/posts/${id}`}
+      passHref>
+      <div className="cursor-pointer group max-w-[400px] min-w-[300px] h-96 rounded-3xl flex flex-col flex-1 gap-2 relative shadow-xl dark:shadow-lg shadow-slate-500 dark:shadow-black">
         <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-3xl brightness-75 dark:brightness-50">
-          <PostImage blurCss={blurCss} coverImg={coverImg} />
+          <PostImage
+            blurCss={blurCss}
+            coverImg={coverImg}
+          />
         </div>
         <section className="z-10 w-full h-full flex flex-col justify-end gap-8 px-10 pb-8 text-slate-50">
           {<Tags tags={tags} />}
           <div className="font-bold text-2xl w-full line-clamp-2 sm:line-clamp-1 md:line-clamp-2">
             {title}
           </div>
-          <div className="text-md w-full line-clamp-2 max-w-2xl">
-            {description}
-          </div>
+          <div className="text-md w-full line-clamp-2 max-w-2xl">{description}</div>
           <div className="text-md italic">{date}</div>
         </section>
       </div>
--- components/PostCardList/PostCardList.tsx
+++ components/PostCardList/PostCardList.tsx
@@ -1,6 +1,6 @@
-import { PostMetaData } from '@lib/posts';
-import { FC } from 'react';
-import PostCard from './PostCard';
+import { PostMetaData } from "@lib/posts";
+import { FC } from "react";
+import PostCard from "./PostCard";
 
 interface Props {
   allPostsMetaData: PostMetaData[];
@@ -8,9 +8,12 @@ interface Props {
 
 const PostCardList: FC<Props> = ({ allPostsMetaData }) => {
   return (
-    <div className="flex flex-wrap gap-8">
+    <div className="flex justify-center flex-wrap gap-8">
       {allPostsMetaData?.map((props) => (
-        <PostCard key={props.id} {...props} />
+        <PostCard
+          key={props.id}
+          {...props}
+        />
       ))}
     </div>
   );
