1631f6bf2b93e1a7a6695c312a66914ad370fcf3
Dongjune Dane Kim <53213397+donggoolosori@users.noreply.github.com>
2023-03-20 21:58:07 +0900

    feat: theme를 localstorage에 저장하지 않는다

--- components/Header/ThemeModeButton/useDarkMode.hook.tsx
+++ components/Header/ThemeModeButton/useDarkMode.hook.tsx
@@ -1,29 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function useDarkMode() {
   const [isDark, setIsDark] = useState<boolean>(true);
 
   useEffect(() => {
-    if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      document.documentElement.classList.add('dark');
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.documentElement.classList.add("dark");
       setIsDark(true);
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove("dark");
       setIsDark(false);
     }
   }, []);
 
   const darkModeHandler = () => {
     if (isDark) {
-      document.documentElement.classList.remove('dark');
-      localStorage.theme = 'light';
+      document.documentElement.classList.remove("dark");
     } else {
-      document.documentElement.classList.add('dark');
-      localStorage.theme = 'dark';
+      document.documentElement.classList.add("dark");
     }
     setIsDark((isDark) => !isDark);
   };
