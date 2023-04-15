84fa18f1db016b750933cd578bd49f74feb24e3d
Dongjune Dane Kim <53213397+donggoolosori@users.noreply.github.com>
2023-04-15 16:34:59 +0900

    feat: Update simtemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,36 @@
 const config = {
   siteUrl: "https://dklog.vercel.app",
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    if (path.includes("tag")) {
+      return null;
+    }
+
+    const defaultConfig = {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+
+    if (path === "/") {
+      return {
+        ...defaultConfig,
+        changefreq: "daily",
+        priority: 1,
+      };
+    }
+
+    if (path.includes("posts/")) {
+      return {
+        ...defaultConfig,
+        changefreq: "monthly",
+        priority: 0.8,
+      };
+    }
+
+    return defaultConfig;
+  },
 };
 
 module.exports = config;
