4b226d68ce2b722220aa4f18b4cefe62a910fede
Dongjune Dane Kim <53213397+donggoolosori@users.noreply.github.com>
2023-06-10 16:25:03 +0900

    feat: add meta to posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,6 +1,17 @@
 import PostsPageMain from "@components/PostsPageMain";
+import { postsPageDescription, postsPageTitle } from "@constants/siteMetaData";
 import { getSortedPostsMetaData, PostMetaData } from "@lib/posts";
 import { getAllTags } from "@lib/tag";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: postsPageTitle,
+  description: postsPageDescription,
+  openGraph: {
+    title: postsPageTitle,
+    description: postsPageDescription,
+  },
+};
 
 export default async function PostsPage() {
   const allPostsMetaData: PostMetaData[] = await getSortedPostsMetaData();
