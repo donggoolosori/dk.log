cff7fcc7544945d821e97278e40e70b3bc70a709
Dongjune Dane Kim <53213397+donggoolosori@users.noreply.github.com>
2023-06-10 16:02:14 +0900

    feat: upgrade daisyui

--- components/Header/ThemeModeButton/ThemeModeButton.tsx
+++ components/Header/ThemeModeButton/ThemeModeButton.tsx
@@ -1,23 +1,31 @@
 "use client";
 
-import { HiSun } from "@react-icons/all-files/hi/HiSun";
-import { HiMoon } from "@react-icons/all-files/hi/HiMoon";
 import useDarkMode from "@hooks/useDarkMode.hook";
+import { HiMoon } from "@react-icons/all-files/hi/HiMoon";
+import { HiSun } from "@react-icons/all-files/hi/HiSun";
 
 const ThemeModeButton = () => {
   const { theme, toggleTheme } = useDarkMode();
 
   return (
-    <button
-      onClick={toggleTheme}
-      className="group p-1 flex items-center justify-center"
-      aria-label="Toggle Dark Mode">
-      {theme === "light" ? (
-        <HiMoon className="text-indigo-500" />
-      ) : (
-        <HiSun className="text-orange-400" />
-      )}
-    </button>
+    <label
+      className={`swap swap-rotate ${theme === "dark" ? "swap-active" : ""}`}>
+      {/* this hidden checkbox controls the state */}
+      <input type="checkbox" onClick={toggleTheme} />
+
+      <HiSun className="text-orange-400 swap-on" size={28} />
+      <HiMoon className="text-indigo-500 swap-off" size={28} />
+    </label>
+    // <button
+    //   onClick={toggleTheme}
+    //   className="group p-1 flex items-center justify-center"
+    //   aria-label="Toggle Dark Mode">
+    //   {theme === "light" ? (
+    //     <HiMoon className="text-indigo-500" />
+    //   ) : (
+    //     <HiSun className="text-orange-400" />
+    //   )}
+    // </button>
   );
 };
 
