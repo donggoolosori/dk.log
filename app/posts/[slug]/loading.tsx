d53e64ef35dba9df13d01cf5973bec598b882395
Dongjune Dane Kim <53213397+donggoolosori@users.noreply.github.com>
2023-06-10 20:36:52 +0900

    feat: add loading to post page

---
+++ app/posts/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+function loading() {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="loading loading-spinner loading-lg" />
+    </div>
+  );
+}
+
+export default loading;
